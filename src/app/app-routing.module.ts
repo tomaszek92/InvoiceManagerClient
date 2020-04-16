0c705f6978ca0163bf3bcc54af0c35302c505a28
Tomasz Tomczykiewicz <tomaszek92@gmail.com>
2020-04-16 17:44:12 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientDetailsComponent } from './client-details/client-details.component';
+import { ClientsListComponent } from './clients-list/clients-list.component';
+import { InvoiceDetailsComponent } from './invoice-details/invoice-details.component';
+import { InvoicesListComponent } from './invoices-list/invoices-list.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'clients', component: ClientsListComponent },
+  { path: 'clients/:id', component: ClientDetailsComponent },
+  { path: 'invoices', component: InvoicesListComponent },
+  { path: 'invoices/:id', component: InvoiceDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InvoicesListComponent } from './invoices-list/invoices-list.component';
+import { InvoiceDetailsComponent } from './invoice-details/invoice-details.component';
+import { ClientsListComponent } from './clients-list/clients-list.component';
+import { ClientDetailsComponent } from './client-details/client-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InvoicesListComponent,
+    InvoiceDetailsComponent,
+    ClientsListComponent,
+    ClientDetailsComponent
   ],
   imports: [
     BrowserModule,
